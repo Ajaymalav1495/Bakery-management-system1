60886e3bc2c0100696fb2032039bcd60f57b5d9d
Ajay malav <malava093@gmail.com>
2024-10-21 22:18:26 +0530

    Home.py

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def home_page():
+    st.title("Home Page")
+    st.write("---")
+    st.write("Welcome to the Home Page!")
+    st.write("---")
+    st.image('thumb-1920-396345.jpg')
+    st.write("---")
